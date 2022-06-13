c366084a4b126490391b4bb2b1b5ea6fe449bef2
unknown <alireza2000masha@gmail.com>
2022-06-14 00:09:59 +0430

    'modal'

--- components/Header.js
+++ components/Header.js
@@ -1,36 +1,64 @@
-import { useContext } from 'react'
-import Image from 'next/image'
-import { ForSearchClick } from '../pages/contexts/forSearchClick'
-import headerStyle from '../styles/headerStyle.module.css'
-import Search from './Search'
+import { useContext, useState } from "react";
+import Image from "next/image";
+import { ForSearchClick } from "../pages/contexts/forSearchClick";
+import headerStyle from "../styles/headerStyle.module.css";
+import Search from "./Search";
+import { Modal } from "react-bootstrap";
 
-export default function Header({photoes}) {
-  const {closeSearch} = useContext(ForSearchClick)
-  const {openSearch} = useContext(ForSearchClick)
+export default function Header({ photoes }) {
+  const { closeSearch } = useContext(ForSearchClick);
+  const { openSearch } = useContext(ForSearchClick);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleSHowModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
 
   return (
     <div className={headerStyle.header} onClick={closeSearch}>
-        <div className={headerStyle.pintrestIcon}>
-            <Image src='/pintrestIcon.png' width={25} height={25}/>
+      <div className={headerStyle.pintrestIcon}>
+        <Image src="/pintrestIcon.png" width={25} height={25} />
+      </div>
+      <button className={headerStyle.homeButton}>Home</button>
+      <Search onClick={openSearch} photoes={photoes} />
+      <div className={headerStyle.headerRight}>
+        <div className={headerStyle.alertIcon}>
+          <Image src="/alertIcon.png" width={25} height={25} />
         </div>
-        <button className={headerStyle.homeButton}>Home</button>
-        <Search onClick={openSearch} photoes={photoes}/>
-        <div className={headerStyle.headerRight} >
-          <div className={headerStyle.alertIcon}>
-            <Image src='/alertIcon.png' width={25} height={25}/>
-          </div>
-          <div className={headerStyle.messageIcon}>
-            <Image src='/messageIcon.png' width={25} height={25}/> 
-          </div>
-          <div className={headerStyle.profIcon}>
-            <span></span>
-          </div>
-          <div className={headerStyle.menu}>
-            <Image src='/downArrow.png' width={25} height={25}/> 
-          </div>
+        <div className={headerStyle.messageIcon}>
+          <Image src="/messageIcon.png" width={25} height={25} />
         </div>
+        <div className={headerStyle.profIcon}>
+          <span></span>
+        </div>
+        <div className={headerStyle.menu}>
+          <Image
+            src="/downArrow.png"
+            width={25}
+            height={25}
+            onClick={handleSHowModal}
+          />
+          <Modal
+            className="openMenu"
+            show={showModal}
+            onHide={handleCloseModal}
+            backdropClassName={headerStyle.backdrop}
+            dialogClassName={headerStyle.menuDialog}
+          >
+            <Modal.Body className={headerStyle.menuModalBody}>
+              <p>Your accounts</p>
+              <div>Add accounts</div>
+              <div>Convert to business</div>
+              <p className={headerStyle.moreOp}>More option</p>
+              <div>Setting</div>
+              <div>Tune your home feed</div>
+              <div>Install the Windows app</div>
+              <div>Get help</div>
+              <div>See terms and privacy</div>
+              <div>Log out</div>
+            </Modal.Body>
+          </Modal>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-
-
--- pages/photo/[id]/index.js
+++ pages/photo/[id]/index.js
@@ -15,6 +15,8 @@ export default function photo({photo}) {
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.28.4/sweetalert2.min.css" />
                 <script defer src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.28.4/sweetalert2.all.min.js" integrity="sha256-qtyU+b249rw/5PQ1KXGRtxjlgg6hfU2EK50YOlc0n50=" crossorigin="anonymous"></script>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"/>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
             </Head>
             <ForSearchProvider>
                 <ForSearchClickProvider>
